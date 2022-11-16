498cdbd540ccd15c274e925667226f142f166c49
Jakob Rönström <jakob@ronstrom.net>
2022-11-16 15:59:30 +0100

    Added file parsing functionality and some test cases

--- day01/Sources/day01/App.swift
+++ day01/Sources/day01/App.swift
@@ -2,21 +2,40 @@ import Foundation
 
 @main
 public struct App {
+    let input: [Int]
+    
+    init(_ input: [Int]) {
+        self.input = input
+    }
+    
     public static func main() {
         let part = ProcessInfo.processInfo.environment["part"] ?? "part1"
+        let input = parse(file: "input.txt")
         
         if part == "part2" {
-            print(App().getSolutionPart2())
+            print(App(input).getSolutionPart2())
         } else  {
-            print(App().getSolutionPart1())
+            print(App(input).getSolutionPart1())
         }
     }
     
-    private func getSolutionPart1() -> Int {
-        return 1
+    static func parse(file filename: String) -> [Int] {
+        do {
+            let content = try String(contentsOfFile: filename)
+            
+            return content.components(separatedBy: .newlines).compactMap{Int($0)}
+        } catch {
+            print("Error reading/parsing \(filename) - \(error)")
+        }
+        
+        return [Int]()
+    }
+    
+    func getSolutionPart1() -> Int {
+        return input.reduce(0, +)
     }
 
-    private func getSolutionPart2() -> Int {
-        return 2
+    func getSolutionPart2() -> Int {
+        return input.reduce(1, *)
     }
 }
